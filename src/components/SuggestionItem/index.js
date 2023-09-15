74c3f96e7156c3ac73564e445a64e14ee7932f90
BOJJA RAJI <94085980+BOJJRAJI@users.noreply.github.com>
2023-09-15 11:59:04 +0530

    Update index.js

--- src/components/SuggestionItem/index.js
+++ src/components/SuggestionItem/index.js
@@ -1,13 +1,6 @@
 import './index.css'
 
 const SuggestionItem = props => {
-  const {suggestionDetails, getSuggestion} = props
-  const {suggestion} = suggestionDetails
-
-  function sendSuggestion() {
-    getSuggestion(suggestionimport './index.css'
-
-const SuggestionItem = props => {
   const {item, sendSuggestion} = props
   const {suggestion} = item
 
@@ -22,23 +15,6 @@ const SuggestionItem = props => {
         <img
           src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
           className="icon"
-          alt="arrow"
-        />
-      </button>
-    </li>
-  )
-}
-
-export default SuggestionItem
-
-  }
-  return (
-    <li className="suggestion-list">
-      <p className="suggestion">{suggestion}</p>
-      <button className="button" type="button" onClick={sendSuggestion}>
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
-          className="arrow-icon"
           alt="arrow"
         />
       </button>
